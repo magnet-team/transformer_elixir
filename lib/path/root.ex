1a46a62400fd868c007613a3ef3c3b48b2afe026
TJ Stankus <tjstankus@gmail.com>
2020-04-28 15:16:35 -0400

    mix format

--- lib/path/root.ex
+++ lib/path/root.ex
@@ -3,7 +3,7 @@ defmodule Transformerex.Path.Root do
 
   def transform(path: path, target: target, json: json) do
     normalized_path = if String.match?(target, ~r/\A\$\./), do: target, else: "#{path}#{target}"
-    {:ok, value} = Warpath.query json, normalized_path
+    {:ok, value} = Warpath.query(json, normalized_path)
     value
   end
 end
--- lib/transformerex.ex
+++ lib/transformerex.ex
@@ -7,22 +7,22 @@ defmodule Transformerex do
 
   def transform(file_path \\ @yaml_test_file, root \\ @json_root_path) do
     {:ok, [yaml]} = YamlElixir.read_all_from_file(file_path, atoms: true)
-    operate yaml["data"], yaml["mapping"], root
+    operate(yaml["data"], yaml["mapping"], root)
   end
 
   defp operate(json, mapping, root) do
-    Enum.reduce mapping, %{}, fn({target, new_mapping}, result) ->
+    Enum.reduce(mapping, %{}, fn {target, new_mapping}, result ->
       Map.put(
         result,
         target,
         Path.transform(
           mapping: new_mapping,
-          path:    fully_qualified_root(root),
-          target:  target,
-          json:    json
+          path: fully_qualified_root(root),
+          target: target,
+          json: json
         )
       )
-    end
+    end)
   end
 
   defp fully_qualified_root(root) do
